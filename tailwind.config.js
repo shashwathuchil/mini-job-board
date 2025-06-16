e63ec5810c1e245f46987fe81d68c4da0a868ed8
shashwathuchil <shashwathamruthuchil@gmail.com>
2025-06-16 18:33:11 +0530

    layout css code updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ankr-primary': '#3498db',
+        'ankr-secondary': '#f1c40f',
+        'ankr-success': '#2ecc71',
+        'ankr-danger': '#e74c3c',
+        'ankr-warning': '#f39c12',
+        'ankr-info': '#1abc9c',
+      }
+    }
   },
   plugins: [],
 }
